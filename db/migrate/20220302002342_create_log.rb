e6808a6076f38d8b4b5da89793cc637921b83511
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2022-03-01 21:58:37 -0300

    create Player and Log models

---
+++ db/migrate/20220302002342_create_log.rb
@@ -0,0 +1,14 @@
+class CreateLog < ActiveRecord::Migration[6.1]
+  def change
+    create_table :logs do |t|
+      t.text :file_text
+      t.date :moment
+      t.integer :type, default: 0
+      t.integer :match_id
+      t.integer :murder_id
+      t.integer :victim_id
+      t.string :death_reason
+      t.timestamps
+    end
+  end
+end
